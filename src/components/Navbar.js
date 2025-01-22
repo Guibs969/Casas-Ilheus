4572cf9b7185e07c0a8f8fd0861a03fa0b45fc9e
Guilherme Ferreira Brandao (SE/STIC/CGSA) <guilhermefbrandao@mec.gov.br>
2025-01-22 11:21:43 -0300

    CorreçãoBug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,19 @@ import {Link} from 'react-router-dom'
 
 
 import styles from './Navbar.module.css'
-import logo from '../../img/costs_logo.png'
+import logo from '../img/costs_logo.png'
 
 function Navbar(){
     return(
     <div class={styles.navbar}>
       
-            <Link to= "/">
+            <Link to= "/Casas-Ilheus">
                 <img src={logo} alt="Casas Ilheus" style={{ width: '120px'}} />
                 </Link>
       <ul class = {styles.list}>
             <li className={styles.item}>
 
-            <Link to="/">Home</Link>
+            <Link to="/Casas-Ilheus">Home</Link>
             </li>
            
               <li className={styles.item}>
